95b5013a1e9de0debaee6b8c8c64cb6b3e9ea25e
Parkduksung <duksung1234@naver.com>
2021-03-06 16:19:37 +0900

    [전략패턴] 기본세팅.

---
+++ app/src/main/java/com/example/studydesignpattern/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.studydesignpattern
+
+import android.os.Bundle
+import androidx.annotation.LayoutRes
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity(@LayoutRes private val layoutId: Int) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layoutId)
+    }
+}
---
+++ app/src/main/java/com/example/studydesignpattern/category/StrategyPatternActivity.kt
@@ -0,0 +1,7 @@
+package com.example.studydesignpattern.category
+
+import com.example.studydesignpattern.BaseActivity
+import com.example.studydesignpattern.R
+
+class StrategyPatternActivity : BaseActivity(R.layout.activity_strategy) {
+}
