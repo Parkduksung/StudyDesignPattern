a84053dfccb92cfd488f16da759965b702b696f7
Parkduksung <duksung1234@naver.com>
2021-03-09 23:15:37 +0900

    [전략패턴] 위키 참고하여 youtube UML 그리기.

--- app/src/main/java/com/example/studydesignpattern/category/stratege/StrategyPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/category/stratege/StrategyPatternActivity.kt
@@ -1,14 +1,25 @@
 package com.example.studydesignpattern.category.stratege
 
 import android.os.Bundle
+import android.util.Log
 import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
 import com.example.studydesignpattern.databinding.ActivityStrategyBinding
 
 class StrategyPatternActivity : BaseActivity<ActivityStrategyBinding>(R.layout.activity_strategy) {
 
+    private val redDuck by lazy { RedDuck() }
+    private val blueDuck by lazy { BlueDuck() }
+    private val rubberDuck by lazy { ToyRubberDuck() }
+    private val woodDuck by lazy { ToyWoodDuck() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        redDuck.display()
+        blueDuck.display()
+        rubberDuck.display()
+        woodDuck.display()
     }
 }
 
@@ -18,13 +29,13 @@ interface FlyBehavior {
 
 class FlyWithWings : FlyBehavior {
     override fun fly() {
-        TODO("Not yet implemented")
+        Log.d("결과", "FlyWithWings")
     }
 }
 
 class FlyNoWay : FlyBehavior {
     override fun fly() {
-        TODO("Not yet implemented")
+        Log.d("결과", "FlyNoWay")
     }
 }
 
@@ -35,93 +46,100 @@ interface QuackBehavior {
 
 class Quack : QuackBehavior {
     override fun quack() {
-        TODO("Not yet implemented")
+        Log.d("결과", "Quack")
     }
 }
 
 class Squeak : QuackBehavior {
     override fun quack() {
-        TODO("Not yet implemented")
+        Log.d("결과", "Squeak")
     }
 }
 
 class MuteQuack : QuackBehavior {
     override fun quack() {
-        TODO("Not yet implemented")
+        Log.d("결과", "MuteQuack")
     }
 }
 
-abstract class Duck : FlyBehavior, QuackBehavior {
+abstract class Duck {
 
-    fun display() {
+    private lateinit var flyBehavior: FlyBehavior
+    private lateinit var quackBehavior: QuackBehavior
 
-    }
+    abstract fun display()
 
     fun swim() {
-
+        Log.d("결과", "swim")
     }
 
     fun performQuack() {
-
+        quackBehavior.quack()
     }
 
     fun performFly() {
-
+        flyBehavior.fly()
     }
 
-    fun setFlyBehavior() {
-
+    fun setFlyBehavior(flyBehavior: FlyBehavior) {
+        this.flyBehavior = flyBehavior
     }
 
-    fun setQuackBehavior() {
-
+    fun setQuackBehavior(quackBehavior: QuackBehavior) {
+        this.quackBehavior = quackBehavior
     }
-
 }
 
 
 class BlueDuck : Duck() {
-
-    override fun fly() {
-        TODO("Not yet implemented")
+    init {
+        setFlyBehavior(FlyWithWings())
+        setQuackBehavior(Quack())
     }
 
-    override fun quack() {
-        TODO("Not yet implemented")
+    override fun display() {
+        Log.d("결과", "BlueDuck")
+        performFly()
+        performQuack()
     }
-
 }
 
 class RedDuck : Duck() {
-
-    override fun fly() {
-        TODO("Not yet implemented")
+    init {
+        setFlyBehavior(FlyWithWings())
+        setQuackBehavior(Quack())
     }
 
-    override fun quack() {
-        TODO("Not yet implemented")
+    override fun display() {
+        Log.d("결과", "RedDuck")
+        performFly()
+        performQuack()
     }
 }
 
 class ToyRubberDuck : Duck() {
-
-    override fun fly() {
-        TODO("Not yet implemented")
+    init {
+        setFlyBehavior(FlyNoWay())
+        setQuackBehavior(Squeak())
     }
 
-    override fun quack() {
-        TODO("Not yet implemented")
+    override fun display() {
+        Log.d("결과", "ToyRubberDuck")
+        performFly()
+        performQuack()
     }
 }
 
 class ToyWoodDuck : Duck() {
-
-    override fun fly() {
-        TODO("Not yet implemented")
+    init {
+        setFlyBehavior(FlyWithWings())
+        setQuackBehavior(MuteQuack())
     }
 
-    override fun quack() {
-        TODO("Not yet implemented")
+    override fun display() {
+        Log.d("결과", "ToyWoodDuck")
+        performFly()
+        performQuack()
     }
 }
 
