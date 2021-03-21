fffd10befd184a30b0ce3dab931b5e0ca0355a56
Parkduksung <duksung1234@naver.com>
2021-03-21 17:26:12 +0900

    [관찰자패턴-wiki] 초기 세팅.

---
+++ app/src/main/java/com/example/studydesignpattern/category/observer/ObserverPatternActivity.kt
@@ -0,0 +1,14 @@
+package com.example.studydesignpattern.category.observer
+
+import android.os.Bundle
+import com.example.studydesignpattern.BaseActivity
+import com.example.studydesignpattern.R
+import com.example.studydesignpattern.databinding.ActivityWikiObserverBinding
+
+class ObserverPatternActivity : BaseActivity<ActivityWikiObserverBinding>(R.layout.activity_wiki_observer) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
