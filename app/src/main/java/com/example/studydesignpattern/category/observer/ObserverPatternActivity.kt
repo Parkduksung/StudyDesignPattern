a9f5b9e1c628073c739f362d36590a786e7ade5c
Parkduksung <duksung1234@naver.com>
2021-03-26 18:22:29 +0900

    [관찰자패턴] 강의 UML

--- app/src/main/java/com/example/studydesignpattern/category/observer/ObserverPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/category/observer/ObserverPatternActivity.kt
@@ -1,14 +1,72 @@
 package com.example.studydesignpattern.category.observer
 
 import android.os.Bundle
+import android.util.Log
 import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
 import com.example.studydesignpattern.databinding.ActivityWikiObserverBinding
 
 class ObserverPatternActivity : BaseActivity<ActivityWikiObserverBinding>(R.layout.activity_wiki_observer) {
 
+    private val sampleObserver1: Observer = SampleObserver1()
+    private val sampleObserver2: Observer = SampleObserver2()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val concreteSubject = ConcreteSubject().apply {
+            registerObserver(sampleObserver1)
+            registerObserver(sampleObserver2)
+        }
+
+        concreteSubject.notifyObserver()
+
+        concreteSubject.removeObserver(sampleObserver1)
+
+        concreteSubject.notifyObserver()
+
+
+    }
+}
+
+
+interface Observer {
+    fun update()
+}
+
+class SampleObserver1 : Observer {
+    override fun update() {
+        Log.d("결과", "SampleObserver1")
+    }
+}
+
+class SampleObserver2 : Observer {
+    override fun update() {
+        Log.d("결과", "SampleObserver2")
+    }
+}
+
+interface Subject {
+    fun registerObserver(observer: Observer)
+    fun removeObserver(observer: Observer)
+    fun notifyObserver()
+}
+
+class ConcreteSubject : Subject {
+
+    private val hashMap = HashMap<Int, Observer>()
+
+    override fun registerObserver(observer: Observer) {
+        hashMap[observer.hashCode()] = observer
+    }
+
+    override fun removeObserver(observer: Observer) {
+        hashMap.remove(observer.hashCode())
+    }
+
+    override fun notifyObserver() {
+        hashMap.forEach {
+            it.value.update()
+        }
     }
 }
