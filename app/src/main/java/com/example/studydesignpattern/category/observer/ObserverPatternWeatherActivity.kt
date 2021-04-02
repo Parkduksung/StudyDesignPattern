89aba5e73940c406693a15b9e83b7ee90294a5de
Parkduksung <duksung1234@naver.com>
2021-04-03 00:57:02 +0900

    [관찰자패턴] 오늘한것을 나만의 방식으로 해석.

--- app/src/main/java/com/example/studydesignpattern/category/observer/ObserverPatternWeatherActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/category/observer/ObserverPatternWeatherActivity.kt
@@ -1,5 +1,6 @@
 package com.example.studydesignpattern.category.observer
 
+import android.os.Bundle
 import android.util.Log
 import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
@@ -8,6 +9,20 @@ import com.example.studydesignpattern.databinding.ActivityWikiObserverBinding
 class ObserverPatternWeatherActivity :
     BaseActivity<ActivityWikiObserverBinding>(R.layout.activity_wiki_observer) {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val currentConditionDisplay1: WeatherObserver = CurrentConditionDisplay(Weather("10","10","20"))
+        val currentConditionDisplay2: WeatherObserver = CurrentConditionDisplay(Weather("11","11","21"))
+        val currentConditionDisplay3: WeatherObserver = CurrentConditionDisplay(Weather("12","12","22"))
+
+        val weatherData: WeatherSubject = WeatherData()
+        weatherData.registerObserver(currentConditionDisplay1)
+        weatherData.registerObserver(currentConditionDisplay2)
+        weatherData.registerObserver(currentConditionDisplay3)
+
+        weatherData.notifyObserver()
+    }
 
 }
 
@@ -22,45 +37,38 @@ interface WeatherSubject : WeatherObserver {
     fun notifyObserver()
 }
 
+class CurrentConditionDisplay(private val weather: Weather) : WeatherObserver {
+    override fun update() {
+        Log.d("결과", "온도 : ${weather.temperature} 습도 : ${weather.humidity} 압력 : ${weather.pressure}")
+    }
+}
+
 
 class WeatherData : WeatherSubject {
+
+    private val weatherObserverSetList = mutableSetOf<WeatherObserver>()
+
     override fun registerObserver(weatherObserver: WeatherObserver) {
-        Log.d("결과", "registerObserver")
+        weatherObserverSetList += weatherObserver
     }
 
     override fun removeObserver(weatherObserver: WeatherObserver) {
-        Log.d("결과", "removeObserver")
+        weatherObserverSetList -= weatherObserver
     }
 
     override fun notifyObserver() {
-        Log.d("결과", "notifyObserver")
-    }
-
-    fun getTemperature() {
-
-    }
-
-    fun getHumidity() {
-
-    }
-
-    fun getPressure() {
-
-    }
-
-    fun measurementsChanged() {
-
+        weatherObserverSetList.forEach {
+            it.update()
+        }
     }
 
     override fun update() {
-        notifyObserver()
-    }
-}
-
-
-class CurrentConditionDisplay : WeatherObserver {
-    override fun update() {
-
     }
 
 }
+
+data class Weather(
+    var temperature: String? = "",
+    var humidity: String? = "",
+    var pressure: String? = ""
+)
