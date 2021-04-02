378e7436ae178588e1b0864f2e390881eb2af494
Parkduksung <duksung1234@naver.com>
2021-04-02 23:46:31 +0900

    [관찰자패턴] 오윤님이 알려주신 print 검증 방법

--- app/src/test/java/com/example/studydesignpattern/category/observer/ObserverPatternActivityTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/observer/ObserverPatternActivityTest.kt
@@ -1,5 +1,26 @@
 package com.example.studydesignpattern.category.observer
 
+import org.junit.After
+import org.junit.Before
+import java.io.ByteArrayOutputStream
+import java.io.PrintStream
+
 class ObserverPatternActivityTest {
+
+    private val standardOut: PrintStream? = System.out
+    private val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream()
+
+
+    @Before
+    fun setUp() {
+        System.setOut(PrintStream(outputStreamCaptor))
+    }
+
+
+    @After
+    fun tearDown() {
+        System.setOut(standardOut)
+    }
+
 
 }
