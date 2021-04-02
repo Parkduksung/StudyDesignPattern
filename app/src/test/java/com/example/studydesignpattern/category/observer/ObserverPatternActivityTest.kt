b97900dd94cc97638e292dc2d8acd446e548acc9
Parkduksung <duksung1234@naver.com>
2021-04-02 23:50:18 +0900

    [관찰자패턴] testCase - 빈 옵저버일 경우 아무것도 찍히지 않아야 한다.

--- app/src/test/java/com/example/studydesignpattern/category/observer/ObserverPatternActivityTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/observer/ObserverPatternActivityTest.kt
@@ -1,7 +1,9 @@
 package com.example.studydesignpattern.category.observer
 
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Test
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
@@ -9,6 +11,9 @@ class ObserverPatternActivityTest {
 
     private val standardOut: PrintStream? = System.out
     private val outputStreamCaptor: ByteArrayOutputStream = ByteArrayOutputStream()
+
+    private val sampleObserver1: Observer = SampleObserver1()
+    private val sampleObserver2: Observer = SampleObserver2()
 
 
     @Before
@@ -22,5 +27,14 @@ class ObserverPatternActivityTest {
         System.setOut(standardOut)
     }
 
+
+    @Test
+    fun `빈 옵저버 일 경우, 아무것도 찍히지 않아야 한다`() {
+
+        val concreteSubject = ConcreteSubject()
+        concreteSubject.notifyObserver()
+
+        assertEquals("", outputStreamCaptor.toString())
+    }
 
 }
