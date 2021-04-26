c1da26ff99e48778d573a143bf45d3126bc525e9
Parkduksung <duksung1234@naver.com>
2021-04-26 23:43:26 +0900

    [컴포지트패턴-TDD방식 주석 처리 설명.

--- app/src/test/java/com/example/studydesignpattern/category/composite/CompositePatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/composite/CompositePatternTest.kt
@@ -3,6 +3,11 @@ package com.example.studydesignpattern.category.composite
 import org.junit.Assert.*
 import org.junit.Test
 
+/*
+컴포지트 패턴은 Layout 구성할때, 부모 ViewGroup 에 여러 자식 ViewGroup 혹은 View 들이 추가되는 트리구조일때 구성하면 좋다.
+ */
+
+
 class CompositePatternTest {
 
 
@@ -16,7 +21,7 @@ class CompositePatternTest {
     }
 
     @Test
-    fun `CompositeTextView 의 inflate 의 값은 InflateCompositeTextView 이어야 한다` (){
+    fun `CompositeTextView 의 inflate 의 값은 InflateCompositeTextView 이어야 한다`() {
 
         val compositeTextView = CompositeTextView("CompositeTextView")
 
@@ -25,7 +30,7 @@ class CompositePatternTest {
     }
 
     @Test
-    fun `Composite 에 CompositeTextView 를 add 하고 inflate 하였을때 값이 InflateCompositeTextView 이어야 한다` () {
+    fun `Composite 에 CompositeTextView 를 add 하고 inflate 하였을때 값이 InflateCompositeTextView 이어야 한다`() {
 
         val composite = Composite("Composite")
 
@@ -35,7 +40,7 @@ class CompositePatternTest {
     }
 
     @Test
-    fun `Composite 에 CompositeTextView1, CompositeTextView2 를 add 하고 inflate 하였을때 값이 InflateCompositeTextView1InflateCompositeTextView2 이어야 한다` () {
+    fun `Composite 에 CompositeTextView1, CompositeTextView2 를 add 하고 inflate 하였을때 값이 InflateCompositeTextView1InflateCompositeTextView2 이어야 한다`() {
 
         val composite = Composite("Composite")
 
