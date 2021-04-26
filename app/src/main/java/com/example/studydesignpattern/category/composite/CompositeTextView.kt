5a4d0eb06a93ee82c822a30e5ab101193af34c39
Parkduksung <duksung1234@naver.com>
2021-04-26 23:20:10 +0900

    [컴포지트패턴-TDD방식 Green - CompositeTextView 생성.

---
+++ app/src/main/java/com/example/studydesignpattern/category/composite/CompositeTextView.kt
@@ -0,0 +1,12 @@
+package com.example.studydesignpattern.category.composite
+
+class CompositeTextView {
+    fun getName(): String {
+        return "CompositeTextView"
+    }
+
+    fun inflate(): String {
+        return "InflateCompositeTextView"
+    }
+
+}
