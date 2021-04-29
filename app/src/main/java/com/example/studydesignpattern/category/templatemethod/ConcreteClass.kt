4aa36497acb0b6a190f8b822c1f1fb11c789c12e
Parkduksung <duksung1234@naver.com>
2021-04-29 23:49:59 +0900

    [템플릿패턴-TDD방식] Green - ConcreteClass 관련 테스트케이스 통과

---
+++ app/src/main/java/com/example/studydesignpattern/category/templatemethod/ConcreteClass.kt
@@ -0,0 +1,12 @@
+package com.example.studydesignpattern.category.templatemethod
+
+class ConcreteClass {
+
+    fun hock1() : String {
+        return "ABSTRACT hook1 implementation"
+    }
+
+    fun hock2() : String {
+        return "ABSTRACT hook2 implementation"
+    }
+}
