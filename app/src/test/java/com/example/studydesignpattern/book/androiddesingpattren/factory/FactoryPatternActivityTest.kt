45fbcb3543665c26eb41c4148cae9c8be6d18658
Parkduksung <duksung1234@naver.com>
2021-05-05 20:39:48 +0900

    [안드로이드디자인패턴활용연습책] TDD, Red - 테스트 케이스 구성.

--- app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/factory/FactoryPatternActivityTest.kt
+++ app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/factory/FactoryPatternActivityTest.kt
@@ -2,7 +2,13 @@ package com.example.studydesignpattern.book.androiddesingpattren.factory
 
 import org.junit.Assert.*
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.junit.MockitoJUnitRunner
 
+
+@RunWith(MockitoJUnitRunner::class)
 class FactoryPatternActivityTest {
 
 
@@ -59,6 +65,28 @@ class FactoryPatternActivityTest {
 
         assert(brioche.calories() == 175)
 
+    }
+
+//    petService = Mockito.mock(PetService::class.java)
+//    Mockito.`when`(petService.listPets()).thenReturn(getPetList())
+
+    @Test
+    fun `BreadFactory 의 getBread 의 parameter 값이 Baguette 이면 생성된 Bread 의 이름이 Baguette 이어야 한다`() {
+
+        val breadFactory = Mockito.mock(BreadFactory::class.java)
+        Mockito.`when`(breadFactory.getBread("Baguette")).thenReturn(Baguette())
+
+        assert(breadFactory.name() == "Baguette")
+
+    }
+
+    @Test
+    fun `BreadFactory 의 getBread 의 parameter 값이 Baguette 이면 생성된 Bread 의 칼로리가 150 이어야 한다`() {
+
+        val breadFactory = Mockito.mock(BreadFactory::class.java)
+        Mockito.`when`(breadFactory.getBread("Baguette")).thenReturn(Baguette())
+
+        assert(breadFactory.calories() == 150)
     }
 
 
