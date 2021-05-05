48216f1a625b637d8c1125bc9b96c9189184989f
Parkduksung <duksung1234@naver.com>
2021-05-05 19:28:43 +0900

    [안드로이드디자인패턴활용연습책] setting, recyclerview dependency 추가.

---
+++ app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/DesignPattenrnAndBestPracticeActivity.kt
@@ -0,0 +1,14 @@
+package com.example.studydesignpattern.book.androiddesingpattren
+
+import android.os.Bundle
+import com.example.studydesignpattern.BaseActivity
+import com.example.studydesignpattern.R
+import com.example.studydesignpattern.databinding.ActivityMainBinding
+
+class DesignPatternAndBestPracticeActivity :
+    BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
