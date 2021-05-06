1321fd75b1a44e220583b8547a5aae0be8985f30
Parkduksung <duksung1234@naver.com>
2021-05-06 23:00:43 +0900

    [안드로이드디자인패턴활용연습책] Green, Refactor Bread 부분 테스트케이스 통과 및 구성.

--- app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/builder/BuilderPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/builder/BuilderPatternActivity.kt
@@ -3,11 +3,33 @@ package com.example.studydesignpattern.book.androiddesingpattren.builder
 import android.os.Bundle
 import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
+import com.example.studydesignpattern.book.androiddesingpattren.factory.Bread
 import com.example.studydesignpattern.databinding.ActivityBuilderBinding
 
 class BuilderPatternActivity : BaseActivity<ActivityBuilderBinding>(R.layout.activity_builder) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+}
+
+
+class Bagel : Bread {
+    override fun name(): String {
+        return "Bagel"
+    }
+
+    override fun calories(): Int {
+        return 200
+    }
+}
+
+class Bun : Bread {
+    override fun name(): String {
+        return "Bun"
+    }
+
+    override fun calories(): Int {
+        return 250
     }
 }
