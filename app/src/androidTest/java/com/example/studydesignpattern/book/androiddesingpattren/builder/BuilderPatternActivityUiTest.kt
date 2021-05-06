3eab3d498b8e6fe896ee3441a0547f65583a2eef
Parkduksung <duksung1234@naver.com>
2021-05-06 23:43:15 +0900

    [안드로이드디자인패턴활용연습책] Ui TestClass 생성.

---
+++ app/src/androidTest/java/com/example/studydesignpattern/book/androiddesingpattren/builder/BuilderPatternActivityUiTest.kt
@@ -0,0 +1,18 @@
+package com.example.studydesignpattern.book.androiddesingpattren.builder
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BuilderPatternActivityUiTest {
+
+
+    @Before
+    fun setup() {
+        ActivityScenario.launch(BuilderPatternActivity::class.java)
+    }
+
+}
