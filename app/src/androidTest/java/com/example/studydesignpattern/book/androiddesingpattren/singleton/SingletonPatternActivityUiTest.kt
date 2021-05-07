097c2a254c7e25759179843aa85c4040ee1aec5a
Parkduksung <duksung1234@naver.com>
2021-05-07 20:41:36 +0900

    [안드로이드디자인패턴활용연습책] Red 테스트 케이스 생성.

---
+++ app/src/androidTest/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivityUiTest.kt
@@ -0,0 +1,44 @@
+package com.example.studydesignpattern.book.androiddesingpattren.singleton
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.studydesignpattern.R
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class SingletonPatternActivityUiTest {
+
+
+    @Before
+    fun setup() {
+        ActivityScenario.launch(SingletonPatternActivity::class.java)
+    }
+
+
+    @Test
+    fun shouldShow1EditTextAnd1ButtonWhenAppStart() {
+
+        Espresso.onView(withId(R.id.input_et)).check(matches(isDisplayed()))
+        Espresso.onView(withId(R.id.set_info_button)).check(matches(isDisplayed()))
+
+    }
+
+
+    @Test
+    fun shouldAlrightValueWhenInputEditTextAndClickButton() {
+
+        Espresso.onView(withId(R.id.set_info_button)).perform(click())
+
+        Espresso.onView(withId(R.id.get_data_tv))
+            .check(matches(withText(SingletonPerson.getName())))
+
+    }
+
+}
