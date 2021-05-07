f366b1266f098f7feae101c50bfd126276c7d7d9
Parkduksung <duksung1234@naver.com>
2021-05-07 21:32:34 +0900

    [안드로이드디자인패턴활용연습책] 전략패턴 Red, 테스트케이스 생성.

--- app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/strategy/StrategyPatternActivityTest.kt
+++ app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/strategy/StrategyPatternActivityTest.kt
@@ -1,7 +1,61 @@
 package com.example.studydesignpattern.book.androiddesingpattren.strategy
 
-import org.junit.Assert.*
+import org.junit.Test
 
 class StrategyPatternActivityTest {
+
+
+    @Test
+    fun `소주의 도수는 18도 이어야 한다`() {
+
+        val soju = Soju()
+
+        assert(soju.alcohol() == 18)
+
+    }
+
+    @Test
+    fun `소주의 가격은 3000원 이어야 한다`() {
+
+        val soju = Soju()
+
+        assert(soju.price() == 3000)
+    }
+
+    @Test
+    fun `맥주의 도수는 5도 이어야 한다`() {
+
+        val bear = Bear()
+
+        assert(bear.alcohol() == 5)
+
+    }
+
+    @Test
+    fun `맥주의 가격은 2500원 이어야 한다`() {
+
+        val bear = Bear()
+
+        assert(bear.price() == 2500)
+    }
+
+
+    @Test
+    fun `양주의 도수는 40도 이어야 한다`() {
+
+        val whiskey = Whiskey()
+
+        assert(whiskey.alcohol() == 40)
+
+    }
+
+    @Test
+    fun `양주의 가격은 30000원 이어야 한다`() {
+
+        val whiskey = Whiskey()
+
+        assert(whiskey.price() == 30000)
+    }
+
 
 }
