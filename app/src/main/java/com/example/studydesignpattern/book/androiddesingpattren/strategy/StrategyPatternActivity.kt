3ecc5ad86a3a23868607410e07bdbd69e39ea67d
Parkduksung <duksung1234@naver.com>
2021-05-07 21:34:46 +0900

    [안드로이드디자인패턴활용연습책] 전략패턴 Green, 테스트케이스 통과 성공 및 구성.

--- app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/strategy/StrategyPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/strategy/StrategyPatternActivity.kt
@@ -12,3 +12,38 @@ class StrategyPatternActivity :
         super.onCreate(savedInstanceState)
     }
 }
+
+interface Drink {
+    fun alcohol(): Int
+    fun price(): Int
+}
+
+class Soju : Drink {
+    override fun alcohol(): Int {
+        return 18
+    }
+
+    override fun price(): Int {
+        return 3000
+    }
+}
+
+class Bear : Drink {
+    override fun alcohol(): Int {
+        return 5
+    }
+
+    override fun price(): Int {
+        return 2500
+    }
+}
+
+class Whiskey : Drink {
+    override fun alcohol(): Int {
+        return 40
+    }
+
+    override fun price(): Int {
+        return 30000
+    }
+}
