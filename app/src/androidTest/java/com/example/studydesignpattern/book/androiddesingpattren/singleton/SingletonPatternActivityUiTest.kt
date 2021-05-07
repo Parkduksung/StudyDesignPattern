6180f43755e05f01c2a9f0583454ae5663895413
Parkduksung <duksung1234@naver.com>
2021-05-07 20:55:59 +0900

    [안드로이드디자인패턴활용연습책] 싱글턴으로 저장된 데이터를 다른 화면에서 출력했을때 동일한지 테스트

--- app/src/androidTest/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivityUiTest.kt
+++ app/src/androidTest/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivityUiTest.kt
@@ -2,12 +2,11 @@ package com.example.studydesignpattern.book.androiddesingpattren.singleton
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.studydesignpattern.R
-import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -33,6 +32,9 @@ class SingletonPatternActivityUiTest {
 
     @Test
     fun shouldAlrightValueWhenInputEditTextAndClickButton() {
+        ActivityScenario.launch(SingletonPatternActivity::class.java)
+
+        Espresso.onView(withId(R.id.input_et)).perform(typeText("duksung"), closeSoftKeyboard())
 
         Espresso.onView(withId(R.id.set_info_button)).perform(click())
 
--- app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivity.kt
@@ -1,18 +1,40 @@
 package com.example.studydesignpattern.book.androiddesingpattren.singleton
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
 import com.example.studydesignpattern.databinding.ActivitySingletonBinding
+import com.example.studydesignpattern.databinding.ActivitySubSingletonBinding
 
 class SingletonPatternActivity :
     BaseActivity<ActivitySingletonBinding>(R.layout.activity_singleton) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        findViewById<Button>(R.id.set_info_button).setOnClickListener {
+            SingletonPerson.setName(findViewById<EditText>(R.id.input_et).text.toString())
+
+            startActivity(Intent(this, SingletonSubActivity::class.java))
+
+        }
+
     }
 }
 
+
+class SingletonSubActivity :
+    BaseActivity<ActivitySubSingletonBinding>(R.layout.activity_sub_singleton) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        findViewById<TextView>(R.id.get_data_tv).text = SingletonPerson.getName()
+    }
+}
 
 object SingletonPerson {
 
