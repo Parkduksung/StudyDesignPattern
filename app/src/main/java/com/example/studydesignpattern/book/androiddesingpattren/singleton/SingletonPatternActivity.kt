d8146dd03c4fb0ce0bc5d4a18712c9753bf51349
Parkduksung <duksung1234@naver.com>
2021-05-07 20:34:10 +0900

    [안드로이드디자인패턴활용연습책] Red,Green 테스트 케이스 생성 및 성공. (object 로 만들어 싱글턴 사용.)

--- app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivity.kt
+++ app/src/main/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivity.kt
@@ -5,9 +5,22 @@ import com.example.studydesignpattern.BaseActivity
 import com.example.studydesignpattern.R
 import com.example.studydesignpattern.databinding.ActivitySingletonBinding
 
-class SingletonPatternActivity : BaseActivity<ActivitySingletonBinding>(R.layout.activity_singleton) {
+class SingletonPatternActivity :
+    BaseActivity<ActivitySingletonBinding>(R.layout.activity_singleton) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+    }
+}
+
+
+object SingletonPerson {
+
+    private var name = "Parkduksung"
+
+    fun getName(): String = name
+
+    fun setName(name: String) {
+        this.name = name
     }
 }
--- app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivityTest.kt
+++ app/src/test/java/com/example/studydesignpattern/book/androiddesingpattren/singleton/SingletonPatternActivityTest.kt
@@ -1,5 +1,26 @@
 package com.example.studydesignpattern.book.androiddesingpattren.singleton
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class SingletonPatternActivityTest
+class SingletonPatternActivityTest {
+
+
+    @Test
+    fun `SingletonPerson 의 getName 기본값은 Parkduksung 이어야 한다`() {
+
+        val person = SingletonPerson
+
+        assert(person.getName() == "Parkduksung")
+    }
+
+    @Test
+    fun `SingletonPerson 의 setName 으로 duksung 했을때 getName 값은 duksung 이어야 한다`() {
+        val person = SingletonPerson.apply {
+            setName("duksung")
+        }
+
+        assert(person.getName() == "duksung")
+    }
+
+}
