feaf00c0545d2428c1b5f97657432b418b07da3f
Parkduksung <duksung1234@naver.com>
2021-05-13 20:23:46 +0900

    [발표준비] 프록시패턴 wiki Green - 테스트케이스 통과 및 구성.

--- app/src/main/java/com/example/studydesignpattern/category/proxy/ProxyWikiPattern.kt
+++ app/src/main/java/com/example/studydesignpattern/category/proxy/ProxyWikiPattern.kt
@@ -2,3 +2,38 @@ package com.example.studydesignpattern.category.proxy
 
 class ProxyWikiPattern {
 }
+
+
+interface Image {
+    fun displayImage(): String
+    fun loadImageFromDisk(): String
+}
+
+class RealImage(private val imageName: String) : Image {
+
+    override fun loadImageFromDisk(): String {
+        return "Loading_$imageName"
+    }
+
+
+    override fun displayImage(): String {
+        return "Displaying_$imageName"
+    }
+}
+
+class ProxyImage(private val imageName: String) : Image {
+
+    private lateinit var realImage: RealImage
+
+    override fun displayImage(): String {
+        if (!::realImage.isInitialized) {
+            realImage = RealImage(imageName)
+        }
+        return realImage.displayImage()
+    }
+
+    //todo
+    override fun loadImageFromDisk(): String {
+        TODO("Not yet implemented")
+    }
+}
