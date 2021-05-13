53be310c655205f4841e4ecff2dd127fb85e08ee
Parkduksung <duksung1234@naver.com>
2021-05-13 19:53:43 +0900

    [발표준비] 프록시패턴 Red - 테스트 케이스 생성.

--- app/src/test/java/com/example/studydesignpattern/category/proxy/ProxyPatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/proxy/ProxyPatternTest.kt
@@ -1,7 +1,26 @@
 package com.example.studydesignpattern.category.proxy
 
 import org.junit.Assert.*
+import org.junit.Test
 
 class ProxyPatternTest {
+
+    @Test
+    fun `RealSubject 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
+        val realSubject = RealSubject()
+        assert(realSubject.doAction() == "DoAction_RealSubject")
+    }
+
+    @Test
+    fun `Proxy 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
+        val proxy = Proxy()
+        assert(proxy.doAction == "DoAction_RealSubject")
+    }
+
+    @Test
+    fun `Client 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
+        val client = Client()
+        assert(client.doAction() == "DoAction_RealSubject")
+    }
 
 }
