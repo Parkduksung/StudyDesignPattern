0914db355ecebc0f948e238b6c8d98acc93af521
Parkduksung <duksung1234@naver.com>
2021-06-08 22:28:05 +0900

    [Interpreter] 기본.

--- app/src/main/java/com/example/studydesignpattern/category/interpreter/InterpreterPattern.kt
+++ app/src/main/java/com/example/studydesignpattern/category/interpreter/InterpreterPattern.kt
@@ -2,3 +2,29 @@ package com.example.studydesignpattern.category.interpreter
 
 class InterpreterPattern {
 }
+
+
+interface Expression {
+    fun interpreter(con: String): Boolean
+}
+
+
+class TerminalExpression(private val data: String) : Expression {
+    override fun interpreter(con: String): Boolean {
+        return con.contains(data)
+    }
+}
+
+class OrExpression(private val expr1: Expression, private val expr2: Expression) : Expression {
+
+    override fun interpreter(con: String): Boolean {
+        return expr1.interpreter(con) || expr2.interpreter(con)
+    }
+}
+
+class AndExpression(private val expr1: Expression, private val expr2: Expression) : Expression {
+
+    override fun interpreter(con: String): Boolean {
+        return expr1.interpreter(con) && expr2.interpreter(con)
+    }
+}
