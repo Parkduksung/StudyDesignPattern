180a44420175ea4a3ad22faf4becf7368a462ca0
Parkduksung <duksung1234@naver.com>
2021-06-08 22:36:10 +0900

    [Interpreter] 테스트코드 추가.

--- app/src/test/java/com/example/studydesignpattern/category/interpreter/InterpreterPatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/interpreter/InterpreterPatternTest.kt
@@ -1,5 +1,31 @@
 package com.example.studydesignpattern.category.interpreter
 
 import org.junit.Assert.*
+import org.junit.Test
 
-class InterpreterPatternTest
+class InterpreterPatternTest {
+
+    @Test
+    fun `OrExpression(TerminalExpression("Person1"),TerminalExpression("Person1")) 의 interpreter("Person1") 값은 true 이어야 한다`() {
+
+        val person1 : Expression = TerminalExpression("Person1")
+        val person2 : Expression = TerminalExpression("Person2")
+
+        val orExpression = OrExpression(person1,person2)
+
+        assertTrue(orExpression.interpreter("Person1"))
+    }
+
+    @Test
+    fun `AndExpression(TerminalExpression("Person1"),TerminalExpression("Person1")) 의 interpreter("Person1,Person2") 값은 true 이어야 한다`() {
+
+        val person1 : Expression = TerminalExpression("Person1")
+        val person2 : Expression = TerminalExpression("Person2")
+
+        val andExpression = AndExpression(person1,person2)
+
+        assertTrue(andExpression.interpreter("Person1,Person2"))
+    }
+
+
+}
