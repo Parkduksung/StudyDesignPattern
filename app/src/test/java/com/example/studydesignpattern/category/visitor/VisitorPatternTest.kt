05741c363f2eb72e7319670387ff684b265674d9
Parkduksung <duksung1234@naver.com>
2021-06-29 21:13:37 +0900

    발표 - 비지터 패턴 테스트코드 추가.

--- app/src/test/java/com/example/studydesignpattern/category/visitor/VisitorPatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/visitor/VisitorPatternTest.kt
@@ -1,7 +1,19 @@
 package com.example.studydesignpattern.category.visitor
 
+import org.junit.Test
+
 class VisitorPatternTest {
 
+    @Test
+    fun `버스의 기름이 10이고 운전을 1번 했을때 값이 올바르게 나와야 한다`(){
+
+        val bus = Bus(10).apply {
+            drive()
+        }
+
+        assert(bus.getFuel() == 9)
+        assert(bus.visit(CarViewVisitor()) == "현재 버스의 기름 상태 : 9")
+    }
 }
 
 
@@ -58,6 +70,7 @@ class Truck(fuel: Int) : Car {
     override fun visit(viewVisitor: ViewVisitor): String {
         return viewVisitor.visit(this)
     }
+
 }
 
 class CarViewVisitor : ViewVisitor {
