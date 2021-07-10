61213eb0661204b696797b4b724f967689f2e443
Parkduksung <duksung1234@naver.com>
2021-07-10 21:25:27 +0900

    회사 - 프록시 패턴 연습.

---
+++ app/src/test/java/com/example/studydesignpattern/category/proxy/ProxyPatternExamTest.kt
@@ -0,0 +1,51 @@
+package com.example.studydesignpattern.category.proxy
+
+import kotlinx.coroutines.*
+import org.junit.Test
+
+class ProxyPatternExamTest {
+
+
+    @Test
+    fun `imageType 이 png 일 경우 printImage 값이 print PNG 이어야 한다`() = runBlocking {
+
+        val imageProxy: Image = ImageProxy()
+
+
+        assert(imageProxy.printImage() ==  "Local Image")
+
+        delay(550)
+
+        assert(imageProxy.printImage() ==  "Remote Image")
+
+    }
+}
+
+interface Image {
+    fun printImage(): String
+}
+
+class ImageProxy : Image {
+
+    private var loadImage : Image = LocalImage()
+
+    override fun printImage(): String {
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(500)
+            loadImage = RemoteImage()
+        }
+        return loadImage.printImage()
+    }
+}
+
+class LocalImage : Image {
+    override fun printImage(): String {
+       return  "Local Image"
+    }
+}
+
+class RemoteImage : Image {
+    override fun printImage(): String {
+        return "Remote Image"
+    }
+}
