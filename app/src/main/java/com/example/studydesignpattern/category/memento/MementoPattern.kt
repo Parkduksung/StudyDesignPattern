1a7fce8b4ddbcb64520f7c0a613dcf2fc0017350
Parkduksung <duksung1234@naver.com>
2022-07-29 13:02:23 +0900

    [feature] MementoPatternTest 추가.

--- app/src/main/java/com/example/studydesignpattern/category/memento/MementoPattern.kt
+++ app/src/main/java/com/example/studydesignpattern/category/memento/MementoPattern.kt
@@ -33,24 +33,27 @@ class CareTaker {
 
 }
 
+data class LogMemento(val log: String)
 
-class RawMemento(var state : String) {
+class User(var log: String) {
 
-    private val mementoList = ArrayList<Memento>()
+    fun createLogMemento(): LogMemento = LogMemento(log)
 
-    fun createMemento(): Memento {
-        return Memento(state)
+    fun restore(logMemento: LogMemento) {
+        log = logMemento.log
+    }
+}
+
+class LogCollect {
+
+    private val logMementoList = ArrayList<LogMemento>()
+
+    fun saveLog(logMemento: LogMemento) {
+        logMementoList.add(logMemento)
     }
 
-    fun restore(memento: Memento) {
-        state = memento.state
+    fun restore(index: Int): LogMemento {
+        return logMementoList[index]
     }
 
-    fun saveState(state: Memento) {
-        mementoList.add(state)
-    }
-
-    fun restore(index: Int): Memento {
-        return mementoList[index]
-    }
 }
--- app/src/test/java/com/example/studydesignpattern/category/memento/MementoPatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/memento/MementoPatternTest.kt
@@ -7,53 +7,78 @@ class MementoPatternTest {
 
 
     @Test
-    fun `MementoTest`() {
+    fun `User("initial Log") 의 log 값은 "initial Log" 이어야 한다`() {
 
-        val originator = Originator("initial state")
-        val careTaker = CareTaker()
+        val user = User("initial Log")
 
-        careTaker.saveState(originator.createMemento())
-
-        originator.state = "State 1"
-
-        careTaker.saveState(originator.createMemento())
-
-        originator.state = "State 2"
-
-        assertEquals(originator.state, "State 2")
-
-        originator.restore(careTaker.restore(1))
-
-        assertEquals(originator.state, "State 1")
-
-        originator.restore(careTaker.restore(0))
-
-        assertEquals(originator.state, "initial state")
+        assertEquals(user.log, "initial Log")
 
     }
 
     @Test
-    fun `RawMementoTest`() {
+    fun `User("initial Log") 의 createMemento 의 log 값은 "initial Log" 이어야 한다`() {
 
-        val rawMemento = RawMemento("initial state")
+        val user = User("initial Log")
 
-        rawMemento.saveState(rawMemento.createMemento())
-
-        rawMemento.state = "State 1"
-
-        rawMemento.saveState(rawMemento.createMemento())
-
-        rawMemento.state = "State 2"
-
-        assertEquals(rawMemento.state, "State 2")
-
-        rawMemento.restore(rawMemento.restore(1))
-
-        assertEquals(rawMemento.state, "State 1")
-
-        rawMemento.restore(rawMemento.restore(0))
-
-        assertEquals(rawMemento.state, "initial state")
+        assertEquals(user.createLogMemento().log, "initial Log")
 
     }
+
+    @Test
+    fun `User("initial Log") 의 restore(LogMemento("second Log")) 후의 log 값은 "second Log" 이어야 한다`() {
+
+        val user = User("initial Log")
+
+        user.restore(LogMemento("second Log"))
+
+        assertEquals(user.log, "second Log")
+
+    }
+
+
+    @Test
+    fun `LogCollect 에 User("initial Log") createMemento 를 saveLog 하고 restore(0) log 값은 initial Log 이어야 한다`() {
+
+        val logCollect = LogCollect()
+        val user = User("initial Log")
+
+        logCollect.saveLog(user.createLogMemento())
+
+        assertEquals(logCollect.restore(0).log, "initial Log")
+    }
+
+
+    @Test
+    fun `LogCollect 에 User("initial Log") 가 saveLog 되어있는 상태에서 log값을 "second Log" 로 바꾸고 createMemento 를 saveLog 하고 restore(1) log 값은 second Log 이어야 한다`() {
+
+        val logCollect = LogCollect()
+        val user = User("initial Log")
+        logCollect.saveLog(user.createLogMemento())
+
+        user.log = "second Log"
+
+        logCollect.saveLog(user.createLogMemento())
+
+        assertEquals(logCollect.restore(1).log, "second Log")
+    }
+
+
+    @Test
+    fun `LogCollect 에 User("initial Log") 가 saveLog 되어있는 상태에서 log값을 "second Log" 로 바꾸고 createMemento 를 saveLog 하고 log 값을 "third Log" 바꾸고 앞에 저장하는걸 반복하였을때, logCollect 의 restore(0) 값은 "initial Log" , restore(1) 값은 "second Log" restore(2) 는 "third Log" 이어야 한다`() {
+
+        val logCollect = LogCollect()
+        val user = User("initial Log")
+        logCollect.saveLog(user.createLogMemento())
+
+        user.log = "second Log"
+        logCollect.saveLog(user.createLogMemento())
+
+        user.log = "third Log"
+        logCollect.saveLog(user.createLogMemento())
+
+        assertEquals(logCollect.restore(0).log, "initial Log")
+        assertEquals(logCollect.restore(1).log, "second Log")
+        assertEquals(logCollect.restore(2).log, "third Log")
+    }
+
 }
