0b44bf4263bf9b54d17f9d89ac4c6b6d38d4b76d
Parkduksung <duksung1234@naver.com>
2022-08-03 16:28:24 +0900

    [feature] ProxyPatternTest2 추가.

--- app/src/test/java/com/example/studydesignpattern/category/proxy/ProxyPatternTest.kt
+++ app/src/test/java/com/example/studydesignpattern/category/proxy/ProxyPatternTest.kt
@@ -1,26 +1,62 @@
 package com.example.studydesignpattern.category.proxy
 
-import org.junit.Assert.*
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ProxyPatternTest {
 
     @Test
-    fun `RealSubject 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
-        val realSubject = RealSubject()
-        assert(realSubject.doAction() == "DoAction_RealSubject")
+    fun `NormalFile 의 read(TextFile) 의 값은 "Reading file TextFile" 이어야 한다`() {
+        val normalFile = NormalFile()
+        assertEquals(normalFile.read("TextFile"), "Reading file TextFile")
     }
 
     @Test
-    fun `Proxy 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
-        val proxy = Proxy()
-        assert(proxy.doAction() == "DoAction_RealSubject")
+    fun `SecuredFile 의 password 를 입력하지 않을 경우 read("TextFile") 의 값이 "Incorrect password, Access Denied!" 이어야 한다`() {
+
+        val securedFile = SecuredFile()
+        assertEquals(securedFile.read("TextFile"), "Incorrect password, Access Denied!")
     }
 
     @Test
-    fun `Client 의 doAction 값이 DoAction_RealSubject 이어야 한다`() {
-        val client = Client()
-        assert(client.doAction() == "DoAction_RealSubject")
+    fun `SecuredFile 의 password 가 secret 일 경우 read("TextFile") 의 값이 "Reading file TextFile" 이어야 한다`() {
+
+        val securedFile = SecuredFile()
+        securedFile.password = "secret"
+
+        assertEquals(securedFile.read("TextFile"), "Reading file TextFile")
     }
 
+    @Test
+    fun `SecuredFile 의 password 가 secret 가 아닐 경우 read("TextFile") 의 값이 "Incorrect password, Access Denied!" 이어야 한다`() {
+
+        val securedFile = SecuredFile()
+        securedFile.password = "secret1"
+
+        assertEquals(securedFile.read("TextFile"), "Incorrect password, Access Denied!")
+    }
+
+}
+
+
+interface File {
+    fun read(name: String): String
+}
+
+class NormalFile : File {
+    override fun read(name: String) = "Reading file $name"
+}
+
+class SecuredFile : File {
+    val normalFIle = NormalFile()
+    var password = ""
+
+    override fun read(name: String): String {
+        return if (password == "secret") {
+            println("Password is correct : $password")
+            normalFIle.read(name)
+        } else {
+            "Incorrect password, Access Denied!"
+        }
+    }
 }
